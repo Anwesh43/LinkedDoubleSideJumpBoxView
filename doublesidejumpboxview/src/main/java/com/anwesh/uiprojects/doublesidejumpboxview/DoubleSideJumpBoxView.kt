f5628295d4d80a5ac5ffe5106849e4312f218187
Anwesh43 <anweshthecool0@gmail.com>
2020-04-19 02:27:52 +0530

    adding renderer in view

--- doublesidejumpboxview/src/main/java/com/anwesh/uiprojects/doublesidejumpboxview/DoubleSideJumpBoxView.kt
+++ doublesidejumpboxview/src/main/java/com/anwesh/uiprojects/doublesidejumpboxview/DoubleSideJumpBoxView.kt
@@ -58,14 +58,16 @@ fun Canvas.drawDSJBNode(i : Int , scale : Float, paint : Paint) {
 
 class DoubleSideJumpBoxView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -191,7 +193,7 @@ class DoubleSideJumpBoxView(ctx : Context) : View(ctx) {
         private val djsb : DoubleSideJumpBox = DoubleSideJumpBox(0)
         private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-        fun render(canvas : Canvas, paint : Paint) {
+        fun render(canvas : Canvas) {
             canvas.drawColor(backColor)
             djsb.draw(canvas, paint)
             animator.animate {
