5d3f1684f0ee7ea96c4bb9736ac8ad88a99b0011
Anwesh43 <anweshthecool0@gmail.com>
2020-04-19 01:58:54 +0530

    adding Int and Float extension methods

--- doublesidejumpboxview/src/main/java/com/anwesh/uiprojects/doublesidejumpboxview/DoubleSideJumpBoxView.kt
+++ doublesidejumpboxview/src/main/java/com/anwesh/uiprojects/doublesidejumpboxview/DoubleSideJumpBoxView.kt
@@ -21,3 +21,8 @@ val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#2196F3")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rFactor : Float = 5.6f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
