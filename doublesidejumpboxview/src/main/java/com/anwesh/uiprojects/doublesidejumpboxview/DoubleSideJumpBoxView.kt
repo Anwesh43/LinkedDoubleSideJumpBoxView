63ff40f227e355cbb09ee6f7f8c5ee2215682108
Anwesh43 <anweshthecool0@gmail.com>
2020-04-19 01:56:59 +0530

    adding constants and libraries

--- doublesidejumpboxview/src/main/java/com/anwesh/uiprojects/doublesidejumpboxview/DoubleSideJumpBoxView.kt
+++ doublesidejumpboxview/src/main/java/com/anwesh/uiprojects/doublesidejumpboxview/DoubleSideJumpBoxView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.doublesidejumpboxview
 /**
  * Created by anweshmishra on 19/04/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val delay : Long = 20
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#2196F3")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rFactor : Float = 5.6f
